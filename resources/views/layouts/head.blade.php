5ea2594d389baab9398e6f634c31a226a2e0391b
awimaulana19 <awimaulana19@gmail.com>
2023-06-13 01:49:53 +0800

    Assets

--- resources/views/layouts/head.blade.php
+++
@@ -1,4 +0,0 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/donasien.png') }}" />
-<link rel="stylesheet" href="{{ asset('assets/css/styles.min.css') }}" />
